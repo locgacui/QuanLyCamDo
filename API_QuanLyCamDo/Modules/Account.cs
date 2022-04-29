369613eddf8cb98821fef3e0397b2c9efa5de437
Nguyen Thanh Loc <locgacui@gmail.com>
2022-04-29 11:02:20 +0700

    add api .Net QuanLyCamDo

---
+++ API_QuanLyCamDo/Modules/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_QuanLyCamDo.Modules
+{
+    public class Account
+    {
+        public int id { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public int empID { get; set; }
+    }
+}
